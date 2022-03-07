ec04c123b472c750a7a15b548a8b6877556ab86e
Manoranjan <softmanoranjan@gmail.com>
2022-03-07 07:16:04 +0530

    Create ReadConfig.java

---
+++ src/main/java/com/local/datalake/common/ReadConfig.java
@@ -0,0 +1,42 @@
+package com.local.datalake.common;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Class to load any properties file,
+ * 
+ * by default, it loads /config.properties file
+ */
+public class ReadConfig {
+
+    private static Properties props = null;
+
+    /**
+     * Loads properties
+     * 
+     * @param filePath
+     * @return
+     */
+    public static Properties loadProperties(String filePath) {
+        Properties prop = new Properties();
+        InputStream in = Readconfig.class.getResourceAsStream(filePath);
+        try {
+            prop.load(in);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return prop;
+    }
+
+    /**
+     * Default - loads properties
+     * 
+     * @return
+     */
+    public static Properties loadProperties() {
+        return props != null ? props : (props = loadProperties("/config.properties"));
+    }
+}
