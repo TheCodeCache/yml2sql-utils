f471db2d4174aa79e8b1bf802df071f0c0170ad0
Manoranjan <softmanoranjan@gmail.com>
2022-03-07 07:05:25 +0530

    Create BiField.java

---
+++ src/main/java/com/local/datalake/annotation/BiField.java
@@ -0,0 +1,54 @@
+package com.local.datalake.annotation;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+import com.local.datalake.validator.BiFieldValidator;
+
+/**
+ * cross-field validation involving a pair of fields
+ * 
+ * @author yml2sql
+ */
+@Target({ TYPE, ANNOTATION_TYPE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = BiFieldValidator.class)
+@Documented
+public @interface BiField {
+
+    String message() default "{validation.constraints.bifield.defaultMessage}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    /**
+     * @return The first field
+     */
+    String parent();
+
+    /**
+     * @return The second field
+     */
+    String child();
+
+    /**
+     * Defines several <code>@BiField</code> annotations on the same element
+     *
+     * @see BiField
+     */
+    @Target({ TYPE, ANNOTATION_TYPE })
+    @Retention(RUNTIME)
+    @Documented
+    @interface List {
+        BiField[] value();
+    }
+}
